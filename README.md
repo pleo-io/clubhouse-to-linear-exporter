02c1a3bd617b60251c183190388cfa2c67beba86
Frederic Charette <fredericcharette@gmail.com>
2021-02-11 18:36:12 -0500

    v1

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # clubhouse-to-linear-exporter
 Pulls Clubhouse tickets and creates them back in Linear
+
+## Setup 
+
+You'll need to get a few detail first:
+
+- Your Clubhouse API key from the [settings page](https://app.clubhouse.io/pleo/settings/account/api-tokens)
+
+- Your Linear API key from the [settings page](https://linear.app/pleo/settings/api)
+
+- Fill-in the workflow ids and Linear type mapping by making these curl requests:
+
+
+First to get your Linear team uuid:
+
+```bash
+curl \
+  -X POST \
+  -H "Content-Type: application/json" \
+  -H "Authorization: <your linear api key>" \
+  --data '{ "query": "{ teams { nodes { id name } } }" }' \
+  https://api.linear.app/graphql
+```
+
+Then, with the team id, collect the labels and states:
+
+```bash
+curl \
+  -X POST \
+  -H "Content-Type: application/json" \
+  -H "Authorization: <your linear api key>" \
+  --data '{ "query": "team(id: "<your_team_id>") { labels { nodes { id name } } states { nodes { id name } } }" }' \
+  https://api.linear.app/graphql
+
+```
+
+Update the settings in the code and you are good to go!
+
+
+## Running
+
+```bash
+
+CLUBHOUSE_API_TOKEN=<your clubhouse token> LINEAR_API_TOKEN=<your linear token> TEAM_NAME=<team name> node .
+```
+
+The script will run through all open tickets for that team and sequentially ask if this ticket should be migrated to Linear.
+
+## What's missing
+
+- Screenshots seem broken, Linear tries to upload from the clubhouse url instead of simply embedding it
+- Assignees
+- Comments
+- Upserting in Linear instead of jsut creating
--- index.js
+++ index.js
@@ -1,44 +1,150 @@
 const axios = require('axios');
 const readline = require('readline');
 
+const { TEAM_NAME, CLUBHOUSE_API_TOKEN, LINEAR_API_TOKEN } = process.env;
+
+const clubHouseUrl = 'https://api.clubhouse.io/api/v3';
+const linearUrl = 'https://api.linear.app/graphql';
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-let CLUBHOUSE_API_TOKEN;
-let CLUBHOUSE_TEAM_NAME;
-let LINEAR_API_TOKEN;
-let LINEAR_TEAM_NAME;
+const teamMap = {
+  phoenix: {
+    clubhouse: {
+      id: '8',
+      states: {
+        500000053: 'todo',
+        500000051: 'in_progress',
+        500000863: 'awaiting_review',
+      },
+    },
+    linear: {
+      id: '176367b8-3474-446b-8a81-07a2b5f4865a',
+      labels: {
+        chore: '288526d2-bca9-4774-aced-adb649dff0dc',
+        bug: '14f2f2b7-c427-4fa0-a91b-248d319dac65',
+        feature: 'c4f28ecc-71e8-48e3-bc87-1cf0a0a47627',
+      },
+      states: {
+        todo: 'a951c950-67b6-452f-ba28-dde08d4649f1',
+        in_progress: 'd9ff6f99-9249-4627-a245-5b3c45c96887',
+        awaiting_review: 'd9ff6f99-9249-4627-a245-5b3c45c96887',
+      },
+    },
+  },
+};
 
 let storiesFound = 0;
+let storiesReviewed = 0;
 let storiesImported = 0;
 
-const clubHouseUrl = 'https://api.clubhouse.io/api/v3';
-const linearUrl
+function sequence(array, operation) {
+  return array.reduce((promiseChain, item, index) => {
+    return promiseChain.then((chainResults) => {
+      return operation(item, index).then((currentResult) => [...chainResults, currentResult]);
+    });
+  }, Promise.resolve([]));
+}
+
+function deferred() {
+  let resolve;
+  let reject;
+  const promise = new Promise((res, rej) => {
+    resolve = res;
+    reject = rej;
+  });
+
+  return { promise, resolve, reject };
+}
 
 function loadStories() {
-  return Promise.all(['feature', 'bug', 'chore'].map((storyType) => {
-    return axios(`${clubHouseUrl}/stories/search?token=${CLUBHOUSE_API_TOKEN}`, { headers: { story_type: storyType } });
-  }));
+  return axios.post(`${clubHouseUrl}/stories/search`, {
+    archived: false,
+    workflow_state_types: ['unstarted', 'started'],
+    project_ids: [teamMap[TEAM_NAME].clubhouse.id],
+  }, {
+    headers: { 'Clubhouse-Token': CLUBHOUSE_API_TOKEN },
+    params: {
+      token: CLUBHOUSE_API_TOKEN,
+    },
+  }).then(null, (err) => console.log(JSON.stringify(err.response.data)));
 }
 
 function createLinearStory(story) {
-  // Format from clubhouse to linear, filter out by team
-  rl.question(`Import this ticket ?\n${'-'.repeat(80)}${JSON.stringify(story)}\n\nY/N\n`, (answer) => {
-    if (answer.toLowerCase() === 'y') {
-      // TODO
-    }
-  });
+  const { promise, resolve, reject } = deferred();
+
+  console.clear();
+  console.log(`Story ${++storiesReviewed} / ${storiesFound}\n\n`);
+
+  axios.get(`${clubHouseUrl}/stories/${story.id}`, {
+    headers: { 'Clubhouse-Token': CLUBHOUSE_API_TOKEN },
+    params: {
+      token: CLUBHOUSE_API_TOKEN,
+    },
+  }).then((storyDetails) => {
+    const currentState = teamMap[TEAM_NAME].clubhouse.states[story.workflow_state_id];
+
+    rl.question(`\t#${story.id}\t[${story.story_type}]\t[${currentState}]\t${story.name}\n\n\t${storyDetails.data.description}\n\n\t${story.app_url}\n\n${'-'.repeat(80)}\nImport this ticket (Y/N)?: `, (answer) => {
+      if (answer[0].toLowerCase() === 'y') {
+        console.log('\nImporting...');
+
+        return axios.post(linearUrl, { query: `
+          mutation {
+            issueCreate(
+              input: {
+                title: ${JSON.stringify(storyDetails.data.name)}
+                description: ${JSON.stringify(storyDetails.data.description + `\n\n Clubhouse Ticket: ${story.app_url}`)}
+                teamId: "${teamMap[TEAM_NAME].linear.id}"
+                stateId: "${teamMap[TEAM_NAME].linear.states[currentState]}"
+                labelIds: ["${teamMap[TEAM_NAME].linear.labels[story.story_type]}"]
+              }
+            ) {
+              success
+              issue {
+                id
+                title
+              }
+          } }
+          `
+        }, { headers: { Authorization: LINEAR_API_TOKEN } })
+          .then(() => {
+            storiesImported++;
+            resolve(story.id);
+          }, (err) => {
+            console.log(err.response.data);
+            process.exit(1);
+          });
+      }
+
+      resolve(story.id);
+    });
+
+  }, reject);
+
+  return promise;
 }
 
 function init() {
-  { TEAM_NAME, CLUBHOUSE_API_TOKEN, LINEAR_API_TOKEN, LINEAR_TEAM_NAME } = process.env;
-  
+  if (!TEAM_NAME) throw new Error(`Missing TEAM_NAME env variable. Must be one of [${Object.keys(teamMap)}]`);
+  if (!CLUBHOUSE_API_TOKEN) throw new Error(`Missing CLUBHOUSE_API_TOKEN env variable.`);
+  if (!LINEAR_API_TOKEN) throw new Error(`Missing LINEAR_API_TOKEN env variable.`);
+
   return loadStories().then((stories) => {
-    storiesFound = stories.length;
-    return Promise.all(stories.map(createLinearStory));
+    storiesFound = stories.data.length;
+    return sequence(stories.data, createLinearStory)
+      .then(() => {
+        console.clear();
+        console.log(`Imported ${storiesImported} stories out of ${storiesFound}`);
+        process.exit();
+      });
   });
 }
+
+process.on('SIGINT', () => {
+  process.exit();
+});
 
 init();
